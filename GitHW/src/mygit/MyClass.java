4b21c47d47788d1d9f78e47295f0228cc8c97966
maratang123 <pig5977@skuniv.ac.kr>
2023-12-23 12:59:45 +0900

    first commit in dev branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,6 +4,8 @@ public class MyClass {
 public void main(String[] args) {
 	System.out.println("Hello Git!");
 	System.out.println("Bye Bye Git!");
+	YourClass your=new YourClass();
+	your.sayHello("Everyone");
 	
 	
 }
