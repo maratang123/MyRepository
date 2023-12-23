0a261efc701a788347934f227ceab5cd3aa54ca1
maratang123 <pig5977@skuniv.ac.kr>
2023-12-23 13:10:05 +0900

    branch'dev _b'

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,13 +4,10 @@ public class MyClass {
 public void main(String[] args) {
 	System.out.println("Hello Git!");
 	System.out.println("Bye Bye Git!");
-<<<<<<< HEAD
 	System.out.println("My changed in master branch");
-	
-=======
 	YourClass your=new YourClass();
 	your.sayHello("Everyone");
->>>>>>> refs/heads/dev
+
 	
 	
 }
