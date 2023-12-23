dcee400a729147f29cfbf174c5c5b1764707e120
maratang123 <pig5977@skuniv.ac.kr>
2023-12-23 13:01:43 +0900

    change in master branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,6 +4,8 @@ public class MyClass {
 public void main(String[] args) {
 	System.out.println("Hello Git!");
 	System.out.println("Bye Bye Git!");
+	System.out.println("My changed in master branch");
+	
 	
 	
 }
