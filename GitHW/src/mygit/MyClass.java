967248c333bc61c501d4288267ccf343840336c7
maratang123 <pig5977@skuniv.ac.kr>
2023-12-23 12:49:43 +0900

    setting a println line

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -3,7 +3,8 @@ package mygit;
 public class MyClass {
 public void main(String[] args) {
 	System.out.println("Hello Git!");
-	System.out.println("This is the 2nd version!");
+	System.out.println("Bye Git!");
+	
 	
 }
 }
